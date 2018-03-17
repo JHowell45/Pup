98fc40d9dc5e36753b9c182b055a4d69d7dd9c40
Unknown <jhowell45@googlemail.com>
2018-03-17 18:08:49 +0000

    Refactoring modules to make it easier to manage application.

--- __init__.py
+++ __init__.py
@@ -1,5 +1,5 @@
 """root file for loading the pupper application."""
 from pupper import cli
-from pupper.utility_commands import clean
-from pupper.scaffold_comands import scaffold
-from pupper.package_commands import package
+from pupper.pupper_commands.utility_commands import clean
+from pupper.pupper_commands.scaffold_comands import scaffold
+from pupper.pupper_commands.package_commands import package
