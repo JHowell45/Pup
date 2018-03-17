7d07fcdd8a94c05310ef2aacd1afa7fdbd24b017
Unknown <jhowell45@googlemail.com>
2018-03-17 18:49:12 +0000

    Updated tests and decorators.

--- pupper/decorator_functions/display_decorators.py
+++ pupper/decorator_functions/display_decorators.py
@@ -1,25 +1,28 @@
-"""
-"""
+"""Decorator unctions for displaying commands."""
 from functools import wraps
 from shutil import get_terminal_size
+
 import click
 
 
 def command_handler(command_title, colour='green'):
-    """Use this decorator for
-    """
+    """Use this decorator for surrounding the functions with banners."""
     def decorator(function):
-        """
-        """
+        """Nested decorator function."""
+        terminal_width = int(get_terminal_size()[0])
+        title = ' {} '.format(command_title)
+        banner_length = int((terminal_width - len(title)) / 2)
+        banner = '-' * banner_length
+        command_banner = '|{0}{1}{0}|'.format(
+            banner, title.title())
+        lower_banner = '|{}|'.format('-' * int(len(command_banner) - 2))
+
         @wraps(function)
         def wrapper(*args, **kwargs):
-            """
-            """
-            terminal_width = get_terminal_size()[0]
-            banner_length = int(int(terminal_width) - (len(command_title) + 2))
-            banner = '-' * int((banner_length - 2) / 2)
-            click.secho('|{0} {1} {0}|'.format(
-                banner, command_title.title()), fg=colour)
-            function(*args, **kwargs)
+            """Nested wrapper function."""
+            click.secho(command_banner, fg=colour)
+            result = function(*args, **kwargs)
+            click.secho(lower_banner, fg=colour)
+            return result
         return wrapper
     return decorator
--- pupper/pupper_commands/utility_commands.py
+++ pupper/pupper_commands/utility_commands.py
@@ -2,15 +2,15 @@
 import click
 
 from pupper import cli
+from pupper.decorator_functions.display_decorators import command_handler
 from pupper.pupper_commands.command_functions.clean_functions import \
     clean_directory
 from tqdm import tqdm
-from pupper.decorator_functions.display_decorators import command_handler
 
 
 @cli.command()
 @click.argument('root_directories', nargs=-1)
-@command_handler('clean')
+@command_handler('clean directories')
 def clean(root_directories):
     """Remove all cached files and directories.
 
