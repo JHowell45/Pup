e22e47a780690ae75229be65ebc184f32dc5d561
Unknown <jhowell45@googlemail.com>
2018-03-18 21:22:00 +0000

    Creating functions for packaging and uploading.

--- puppo/pupper_commands/command_functions/package_functions/pypi_fuctions.py
+++ puppo/pupper_commands/command_functions/package_functions/pypi_fuctions.py
@@ -3,3 +3,22 @@
 This file contains functions used for packaging and uploading python packages
 to the PYPI store.
 """
+
+
+def package_application():
+    """Use this function to package the application.
+
+    This function is the root function, which when called, while create the
+    distribution and upload it to the PYPI store.
+    """
+    pass
+
+
+def __create_package():
+    """Use this function to create the distributed package."""
+    pass
+
+
+def __upload_package():
+    """Use this function to upload the distribution package to the PYPI store."""
+    pass
