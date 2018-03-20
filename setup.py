f9d47a11b33ea3f9d351d2334d0acfa79c6c9cde
Unknown <jhowell45@googlemail.com>
2018-03-20 10:44:42 +0000

    updating setup.

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ here = abspath(dirname(__file__))
 
 setup(
     name='puppo',  # Required
-    version='1.0.0',  # Required
+    version='0.2.0',  # Required
     description=('Scripts and commands for running tasks to make your python '
                  'experience easier.'),  # Required
     author='Jacob Howell',  # Optional
